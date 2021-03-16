b5e2ee8357d28ae8598eb425c4ae97f4c4128bd8
Emile Villette <emile.villette@student.uclouvain.be>
2021-03-16 23:09:57 +0100

    Added possibility to add a user in the database (/User_data/Users/uuid.json)

--- User_data/add_user.py
+++ User_data/add_user.py
@@ -7,14 +7,24 @@ Emile Villette - March 2021
 import uuid
 import json
 import os
-from user_settings import modify_user
 
 
-def add_user_in_db(email, preferences):
+# from user_settings import modify_user
+
+
+def add_user_in_db(email, language, preferences):
     user_unique_id = uuid.uuid5(uuid.NAMESPACE_URL, email)
+    path = "{}/{}.json".format("Users", user_unique_id)
 
-    if os.path.isfile('./{0}.json'.format(str(user_unique_id))):
-        pass
+    if os.path.isfile(path):
+        return "E-mail already registered, please log in to modify preferences"
     else:
-        with open("userdata.json", "w") as file:
-            data = json.load(file)
+        with open(path, "w") as user:
+            with open("exampleuserfile/userdata.json", "r") as template_file:
+                template = json.load(template_file)
+
+            template["uuid"], template["email"], template["language"] = str(user_unique_id), email, language
+            # USER PREFERENCES ENCODING TBI
+            user.write(str(template))
+
+            return True
