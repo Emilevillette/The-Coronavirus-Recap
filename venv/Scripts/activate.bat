0b9cab0a664353bec7322e28343f21aa386f3683
Emile Villette <emile.villette@student.uclouvain.be>
2021-03-20 01:37:45 +0100

    switched to venv environnement to not rely on local installation of python

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\emile\OneDrive\2020-2021\Projet-covid\venv
+set VIRTUAL_ENV=C:\Users\emile\OneDrive\2020-2021\Ronron\Projet_covid\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
