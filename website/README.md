7c235488bdf8ce70afe78e629e333cc8a62d0935
Emile Villette <emile.villette@student.uclouvain.be>
2021-03-30 20:46:11 +0200

    add website draft

---
+++ website/README.md
@@ -0,0 +1,12 @@
+# The Corona Recap Website
+To start the website:
+```
+flask run
+```
+or
+```
+py app.py
+```
+## Required components:
+- Flask server framework
+- Chart.js (install with ``npm install chart.js``)
---
+++ website/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/index.html")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/test.html")
+def test():
+    return render_template("index2.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
