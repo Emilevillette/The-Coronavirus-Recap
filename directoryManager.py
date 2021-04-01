ac5c8ed53114a7aad104f83a67e6bd058c6c1fac
Emile Villette <emile.villette@student.uclouvain.be>
2021-04-01 19:38:39 +0200

    Added historial John Hopkins University data

--- directoryManager.py
+++ directoryManager.py
@@ -9,17 +9,20 @@ import os
 from datetime import date, timedelta
 
 
-def daily_directory(path="", yesterday=False):
+def daily_directory(path="", yesterday=False, choose_date=date.today()):
     """Creates a directory named "aaaa-mm-jj" in /data/aaaa-mm-jj.
 
+    :param choose_date: choose the date (datetime)
     :param yesterday: return yesterday's path
     :param path: a string specifying the path where to create the daily path (defaults as ../data/aaaa-mm-jj
     :return: the path (as a string)
     """
     if yesterday:
-        today = path + 'data/' + str(date.today() - timedelta(days=1))
+        today = path + 'data/' + str(choose_date.today() - timedelta(days=1))
+    elif choose_date:
+        today = path + 'data/' + str(choose_date)
     else:
-        today = path + 'data/' + str(date.today())
+        today = path + 'data/' + str(choose_date.today())
 
     if not os.path.exists(today):
         # Create main folder
@@ -27,13 +30,3 @@ def daily_directory(path="", yesterday=False):
         # Create vaccine sub-folder
         os.makedirs(today + "/vaccine")
     return today
-
-
-def create_directory(path):
-    """Create a directory
-
-    :param path: directory path
-    :return: None
-    """
-    if not os.path.exists(path):
-        os.makedirs(path)
