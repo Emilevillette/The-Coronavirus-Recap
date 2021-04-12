2b49140d0b7d0c7194a0ab4d38fddd76701c9283
Emile Villette <emile.villette@student.uclouvain.be>
2021-04-12 02:19:41 +0200

    functionnal graphs

--- website/static/casesGraph.js
+++ website/static/casesGraph.js
@@ -2,7 +2,7 @@ var ctx = document.getElementById("cases_graph");
 var json = JSON.parse('data/graph_data/graph_data_cases.json'); //(with path)
 var cases_graph = new Chart(ctx, {
     type: 'line',
-    data:{
+    data: {
         labels: Object.keys(json.casescountry),
         datasets: [{
             data: Object.values(json.casescountry)
